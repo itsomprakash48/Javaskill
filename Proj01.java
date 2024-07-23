a633cde7152de4529775f31a22610d2d2de3ada6
Om Prakash <82245391+omprakash84@users.noreply.github.com>
2024-07-23 10:37:23 +0530

    Add files via upload
    My passion for coding is reflected in my first-ever Java project, a calculator application that employs if statements to perform basic arithmetic operations. My commitment to learning and enthusiasm for programming mark the beginning of a promising career in technology.

---
+++ Proj01.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class Proj01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("selective arithmetic operation accroding to their assigned number : ");
+		System.out.println("Addition = 1");
+		System.out.println("Substraction = 2");
+		System.out.println("multiplication = 3");
+		System.out.println("division = 4");
+		
+		System.out.print("Enter Operation no : ");
+		int  number = sc.nextInt();
+		
+		if (number == 1){
+			System.out.println("Addition");
+			System.out.print("A :");
+			int num1 = sc.nextInt();
+			System.out.print("B :");
+			int num2 = sc.nextInt();
+			System.out.println(num1 + "+" + num2 + " = "  +(num1+num2));
+		}
+		
+		if (number == 2){
+			System.out.println("Substraction");
+			System.out.print("A :");
+			int num1 = sc.nextInt();
+			System.out.print("B :");
+			int num2 = sc.nextInt();
+			System.out.println(num1 + "-" + num2 + " = "  +(num1-num2));
+		}
+		
+		if (number == 3){
+			System.out.println("Multiplication");
+			System.out.print("A :");
+			int num1 = sc.nextInt();
+			System.out.print("B :");
+			int num2 = sc.nextInt();
+			System.out.println(num1 + "x" + num2 + " = "  +(num1*num2));
+		}
+		
+		
+		if (number == 4){
+			System.out.println("Division");
+			System.out.print("A :");
+			int num1 = sc.nextInt();
+			System.out.print("B :");
+			int num2 = sc.nextInt();
+			System.out.println(num1 + "÷" + num2 + " = "  +(num1/num2));
+		}
+		
+		
+		else {
+			System.out.println("bahut bubak ho beta tum ");
+		}
+		
+	
+	}
+	
+}m.out.println("bahut bubak ho beta tum ");
+		}
+		
+	
+	}
+	
+}	
+	}
+	
+}}
