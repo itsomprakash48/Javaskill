2ae9516f37935e6bd81f63c8c3a5f803d283a153
Om Prakash <82245391+omprakash84@users.noreply.github.com>
2024-08-03 04:35:59 +0000

    name changes

--- 04Loops/que01.java
+++ 04Loops/que01.java
@@ -1,4 +1,4 @@
-public class Main {
+public class que01 {
 	public static void main(String[] args) {
 		int num = 1;
 		
--- 04Loops/que02.java
+++ 04Loops/que02.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class que02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);				
 		int num = 1;
