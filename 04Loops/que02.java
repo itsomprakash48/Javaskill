6c0632ff8b18ca62fab97b27670830aa8dd75105
Om Prakash <82245391+omprakash84@users.noreply.github.com>
2024-08-03 14:50:25 +0000

    adding comments in que 1&2

--- 04Loops/que02.java
+++ 04Loops/que02.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class que02 {
 	public static void main(String[] args) {
+		// print number from 1  to n 
+		
 		Scanner sc = new Scanner(System.in);				
 		int num = 1;
 		int num2= sc.nextInt();
--- 04Loops/que03.java
+++ 04Loops/que03.java
@@ -1,13 +1,15 @@
 import  java.util.*;
 public class que03 {
 	public static void main(String[] args) {
+
+		// print the sun of n natural number
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter your number :");
 		int n =sc.nextInt();
 		int sum = 0;
 		int i= 1;
 		
-	//	int num3 = num2*(num2+1)/2 ;
+	//	int num3 = num2*(num2+1)/2 ; (mathematical formula)
 		
 		while (i<= n){
 			sum= sum+i;
