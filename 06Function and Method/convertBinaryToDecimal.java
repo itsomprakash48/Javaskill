715b3e767e424cd6ead238778fbe657a3272823d
Om Prakash <82245391+omprakash84@users.noreply.github.com>
2024-08-24 16:30:03 +0000

    Binary to decimal converter

---
+++ 06Function and Method/convertBinaryToDecimal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class convertBinaryToDecimal {
+
+    public static int decimal(int n){
+        int pow =0;
+        int dec = 0;
+
+        while (n>0) {
+            int lastDigi = n%2;
+            dec = dec+lastDigi*(int)Math.pow(2,pow);
+            pow++;
+            n = n/10; 
+        }
+        return dec;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the binary number : ");
+        int num = sc.nextInt();
+        int binnum = decimal(num);
+        System.out.println("The decimal form of " + num +" is :"+binnum);
+    }
+}
