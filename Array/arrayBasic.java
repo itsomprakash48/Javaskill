d7f728b4662713c8bd8d47f91833b5a8b4839638
Om Prakash <82245391+omprakash84@users.noreply.github.com>
2024-08-26 14:42:25 +0000

    Dsa concept day 1

---
+++ Array/arrayBasic.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class arrayBasic {
+
+    public static void main(String[] args) {
+
+        // creating an array
+
+        int mark[]= new int[50];
+        int number[] = {12,13,14};
+        int moreNum[] = {1,11,111,1111};
+
+        String fruit[] = {"mango", "papaya", "orange"};
+
+        System.out.println(moreNum[0]+ number[1]);
+        System.out.println(fruit[2]);
+        System.out.println(mark[1]);
+        
+    }
+    
+}
